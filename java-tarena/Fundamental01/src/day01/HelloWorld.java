bff679ae2bb1bb62d072d561d01433b6eab1b8d8
jiao-shi-yu <jiao_shiyu@icloud.com>
2023-04-07 18:26:45 +0800

    第一天课上完了

---
+++ java-tarena/Fundamental01/src/day01/HelloWorld.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class HelloWorld {
+//    主方法，程序的入口。程序的执行，从main开始
+    public static void main(String[] args) {
+//        输出hello world
+        System.out.println("hello, world");
+//        严格区分大小写
+//        所有符号必须是英文的
+//        每句话必须以分号结尾
+    }
+}
